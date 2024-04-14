d113a6fddc82937b5c9eee46a412228520acc82e
COV-2104 <tsehlo@mail.ru>
2024-04-14 21:23:25 +0300

    Домашгнн задание № 1

---
+++ lesson_react/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Message from "./Message";
+
+const App = () => {
+  return (
+    <div>
+      <Message text="Hello, World!" />
+      <Message text="This is a message." />
+    </div>
+  );
+};
+
+export default App;
---
+++ lesson_react/src/Message.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Message.css";
+
+const Message = (props) => {
+  return <div className="message">{props.text}</div>;
+};
+
+export default Message;
